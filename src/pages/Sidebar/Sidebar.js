c58c7ca314b404e25f26177c05e52e6b8c8a2c85
izzatbey <izzatbey@gmail.com>
2020-10-31 20:16:44 +0700

    Sidebar Completed

--- src/pages/Sidebar/Sidebar.js
+++ src/pages/Sidebar/Sidebar.js
@@ -6,123 +6,103 @@
 import React, { Component } from "react";
 import { Button, Container, Nav, Navbar, Jumbotron } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import logo from "./../../asset/LogoGPutih.png";
-import { FaUser, FaRegListAlt } from 'react-icons/fa';
-import { MdDashboard } from 'react-icons/md';
-import { RiBarChartLine } from 'react-icons/ri';
-import { BsFillInboxesFill } from 'react-icons/bs';
-
+import { FaUser, FaRegListAlt } from "react-icons/fa";
+import { MdDashboard } from "react-icons/md";
+import { RiBarChartLine } from "react-icons/ri";
+import { BsFillInboxesFill } from "react-icons/bs";
+import classes from "./Sidebar.module.css";
 
 class Sidebar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      width: window.innerWidth
+      width: window.innerWidth,
     };
   }
   render() {
-    const sidebarBackground = {
-      backgroundImage: "url(" + this.props.image + ")"
-    };
     return (
       <div
-        id="sidebar"
-        className="sidebar"
+        className={classes.Sidebar}
         data-color={this.props.color}
         data-image={this.props.image}
-        style={{marginTop: "30px"}}
       >
-          {this.props.hasImage ? (
-            <div className="sidebar-background" style={sidebarBackground} />
-          ) : (
-            null
-          )}
-        <div className="logo">
-          <a
-            href="#"
-            className="simple-text logo-mini"
-          >
-            <div className="logo-img" style={{paddingTop:"10px", paddingBottom:"20px", display:"flex", flexDirection:"row"}}>
-              <img style={{width: "50px"}, {height: "50px"}} src={logo} alt="logo_image" />
-              <p style={{WebkitTextFillColor: "white", paddingTop:"20px", paddingLeft:"20px", fontSize:"25px", fontWeight:"bold"}} className="simple-text logo-normal"> Guwudang.in </p>
-            </div>
-          </a>
-          
-          <a
-            href="#"
-            className="simple-text logo-mini"
-          >
-            <div style={{paddingTop: "30px" , display:"flex", flexDirection:"row"}}>
-              <MdDashboard style={{color:"white", fontSize:"35px"}}/>
-              <p style={{WebkitTextFillColor: "white", paddingLeft:"20px", fontSize:"20px"}}>
-                Dashboard
-              </p>
-            </div>
-          </a>
+        <div className={classes.sidebarWrapper}>
+          <div className={classes.textContent}>
+            <a hre className="simple-text logo-mini">
+              <div className={classes.logoImg}>
+                <img
+                  style={({ width: "50px" }, { height: "50px" })}
+                  src={logo}
+                  alt="logo_image"
+                />
+                <p className={classes.textContent}>Guwudang.in</p>
+              </div>
+            </a>
 
-          <a
-            href="#"
-            className="simple-text logo-mini"
-          >
-            <div style={{paddingTop: "30px", display:"flex", flexDirection:"row"}}>
-              <RiBarChartLine style={{color:"white", fontSize:"35px"}}/>
-              <p style={{WebkitTextFillColor: "white", paddingLeft:"20px", fontSize:"20px"}}>
-                Report
-              </p>
-            </div>
-          </a>
-          
-          <p style={{paddingTop: "40px", color:"lightgray", fontSize:"20px"}}>Product</p>
-          
-          <a
-            href="#"
-            className="simple-text logo-mini"
-          >
-            <div style={{paddingTop: "10px", display:"flex", flexDirection:"row"}}>
-              <BsFillInboxesFill style={{color:"white", fontSize:"35px"}}/>
-              <p style={{WebkitTextFillColor: "white", paddingLeft:"20px", fontSize:"20px"}}>
-                Inventory
-              </p>
-            </div>
-          </a>
+            <a href="#" className="simple-text logo-mini">
+              <div className={classes.titleContent}>
+                <MdDashboard style={{ color: "white", fontSize: "35px" }} />
+                <p className={classes.titleText}>
+                  Dashboard
+                </p>
+              </div>
+            </a>
 
-          <a
-            href="#"
-            className="simple-text logo-mini"
-          >
-            <div style={{paddingTop: "30px", display:"flex", flexDirection:"row"}}>
-              <FaRegListAlt style={{color:"white", fontSize:"35px"}}/>
-              <p style={{WebkitTextFillColor: "white", paddingLeft:"20px", fontSize:"20px"}}>
-                Invoice
-              </p>
-            </div>
-          </a>
+            <a href="#" className="simple-text logo-mini">
+              <div className={classes.titleContent}>
+                <RiBarChartLine className={classes.logoSet} />
+                <p className={classes.titleText}>
+                  Report
+                </p>
+              </div>
+            </a>
 
-          <p style={{paddingTop: "40px", color:"lightgray", fontSize:"20px"}}>Business</p>
-          
-          <a
-            href="#"
-            className="simple-text logo-mini"
-          >
-            <div style={{paddingTop: "10px", display:"flex", flexDirection:"row"}}>
-              <FaUser style={{color:"white", fontSize:"35px"}}/>
-              <p style={{WebkitTextFillColor: "white", paddingLeft:"20px", fontSize:"20px"}} href="#">
-                Partners
-              </p>
-            </div>
-          </a>
+            <p className={classes.contentType}>
+              Product
+            </p>
 
-          <a
-            href="#"
-            className="simple-text logo-mini"
-          >
-            <div style={{paddingTop: "30px", display:"flex", flexDirection:"row"}}>
-              <FaUser style={{color:"white", fontSize:"35px"}}/>
-              <p style={{WebkitTextFillColor: "white", paddingLeft:"20px", fontSize:"20px"}}>
-                Account
-              </p>
-            </div>
-          </a>
+            <a href="#" className="simple-text logo-mini">
+              <div className={classes.titleContent}>
+                <BsFillInboxesFill className={classes.logoSet}/>
+                <p className={classes.titleText}>
+                  Inventory
+                </p>
+              </div>
+            </a>
+
+            <a href="#" className="simple-text logo-mini">
+              <div className={classes.titleContent}>
+                <FaRegListAlt className={classes.logoSet}/>
+                <p className={classes.titleText}>
+                  Invoice
+                </p>
+              </div>
+            </a>
+
+            <p className={classes.contentType}>
+              Business
+            </p>
+
+            <a href="#" className="simple-text logo-mini">
+              <div className={classes.titleContent}>
+                <FaUser className={classes.logoSet}/>
+                <p className={classes.titleText}>
+                  Partners
+                </p>
+              </div>
+            </a>
+
+            <a href="#" className="simple-text logo-mini">
+              <div className={classes.titleContent}>
+                <FaUser className={classes.logoSet}/>
+                <p className={classes.titleText}>
+                  Account
+                </p>
+              </div>
+            </a>
+          </div>
         </div>
       </div>
     );
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,26 +1,26 @@
 import React from "react";
-import {Container, Row, Col, Card, Form, Button } from "react-bootstrap";
+import { Container, Row, Col, Card, Form, Button } from "react-bootstrap";
+
 import { withRouter } from "react-router";
 import Sidebar from "./Sidebar/Sidebar";
 import Home from "./Home.js";
-import sidebar from './Sidebar.module.css';
+import sidebar from "./Sidebar/Sidebar.module.css";
+import myNavbar from "./Navbar";
 
-const Dash = props => {
-
-
-    return (
-          <Container fluid>
-                 <Row>
-                     <Col xs={2} className={sidebar.sidebarWrapper}>      
-                       <Sidebar />
-                     </Col>
-                     <Col xs={10} className={sidebar.pageContentWrapper} style={{padding:"0"}}>
-                         <Home />
-                     </Col> 
-                 </Row>
-
-             </Container>
-        );
-  };
-  const Dashboard = withRouter(Dash);
-  export default Dashboard
+const Dash = (props) => {
+  return (
+    <Container fluid>
+      <Row>
+        <Col md={2}>
+          <Sidebar />
+        </Col>
+        <Col md={10} className={sidebar.pageContentWrapper}>
+          <myNavbar />
+          <Home />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+const Dashboard = withRouter(Dash);
+export default Dashboard;
