10894d0c44ddb5c924a4baa6dd6d95d3871071ce
Rifqi Fajar Ramdani <rifqifajar24@gmail.com>
2020-11-11 13:13:44 +0700

    first

---
+++ src/pages/Detail Product/Detail.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {
+    Container,
+    Row,
+    Col,
+  } from "react-bootstrap";
+import Gambar from './../../asset/LogoBiru.png'
+
+export default function Detail() {
+    return (
+        <div>
+            <Container>
+            <Row>
+                <Col>
+                <img src={Gambar)/>
+                </Col>
+                <Col>
+                {/* judul barang */}
+                    <Row>
+                        
+                    </Row>
+                {/* kode barang */}
+                    <Row>
+
+                    </Row>
+                </Col>
+            </Row>
+            </Container>
+        </div>
+    )
+}
